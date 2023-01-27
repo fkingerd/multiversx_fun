6e9f244ffc7325e1efe8fd0e3c11290f92929b90
niftrata <97300862+xsperd@users.noreply.github.com>
2023-01-27 11:29:13 +0200

    Added more details.

--- download-nft-pics.py
+++ download-nft-pics.py
@@ -6,10 +6,10 @@ def get_png_from_nft(erd: str, collection: str):
     x = requests.get(f'https://api.multiversx.com/accounts/{erd}/nfts?size=123&collections={collection}')
 
     to_parse = x.json()
-    # print(to_parse)
     for x in range(0, len(to_parse)):
         img_url = to_parse[x]['media'][0]['url']
         img = Image.open(requests.get(img_url, stream = True).raw)
+        
         if len(img_url) == 88:
             img.save(f'{img_url[-5:]}')
         elif len(img_url) == 89:
@@ -19,8 +19,9 @@ def get_png_from_nft(erd: str, collection: str):
         elif len(img_url) == 91:
             img.save(f'{img_url[-8:]}')
         else:
-            return f'There are 0 {collection} NFTs holded on {erd}.'
+            return f'There are 0 {collection} NFTs held in wallet: {erd}.'
 
 # Example of how to call the function:
 # get_png_from_nft('erd1xuf43l9v4d3lxhfg9ehzh244592gf2an7hmuw9h84gma7j8fdsws60rrqn', 'MRG-1c3ba4')
+# replace the placeholders from below as shown above
 get_png_from_nft(enter_erd_here, enter_nft_collection_here)
